ed0dc0ea6885b341cc4b594405a29460ca159a28
wasay28 <wasayr1@bu.edu>
2025-02-11 11:36:57 -0500

    basic view funcs

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def main(request):
+    
+    template_name = 'restaurant/main.html'
+    
+    context = {
+
+    }
+
+    return render(request, template_name, context)
+
+def order(request):
+    ''''Respond to the URL '', delegate work to a template.'''
+
+    template_name = 'restaurant/order.html'
+
+    # dict of context variables
+    context = {
+
+    }
+    return render(request, template_name, context)  
+
+def confirmation(request):
+    ''''Respond to the URL '', delegate work to a template.'''
+
+    template_name = 'restaurant/confirmation.html'
+    # dict of context variables
+    context = {
+
+    }
+    return render(request, template_name, context) 
