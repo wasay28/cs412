61be5f8c46c333e1e0ce079a4d2d8d9f52ed7f3c
wasay28 <wasayr1@bu.edu>
2025-02-23 20:30:51 -0500

    profile model

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
