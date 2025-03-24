57f85201efec3117b4d7d2b20f1e8acf1c576a71
wasay28 <wasayr1@bu.edu>
2025-03-23 20:26:24 -0400

    admin add friend

--- mini_fb/admin.py
+++ mini_fb/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, StatusMessage, Image, StatusImage
+from .models import Profile, StatusMessage, Image, StatusImage, Friend
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(StatusMessage)
 admin.site.register(Image)
 admin.site.register(StatusImage)
+admin.site.register(Friend)
