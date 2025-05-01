a1d5e0e121678099d25022b071bf39eaa1522a53
wasay28 <wasayr1@bu.edu>
2025-05-01 19:46:02 -0400

    views update

--- project/views.py
+++ project/views.py
@@ -12,20 +12,20 @@ from .forms import JournalEntryForm, LocationForm
 class JournalEntryListView(generic.ListView):
     model = JournalEntry
     context_object_name = 'journalentry_list' # Default is 'object_list'
-    template_name = 'travelbuddy/journalentry_list.html' # Explicitly state template
+    template_name = 'project/journalentry_list.html' # Explicitly state template
     # Optional: Order by most recent
     queryset = JournalEntry.objects.order_by('-created_date')
 
 class JournalEntryDetailView(generic.DetailView):
     model = JournalEntry
     context_object_name = 'journalentry' # Default is 'object' or 'journalentry'
-    template_name = 'travelbuddy/journalentry_detail.html'
+    template_name = 'project/journalentry_detail.html'
 
 class JournalEntryCreateView(LoginRequiredMixin, generic.CreateView):
     model = JournalEntry
     form_class = JournalEntryForm 
-    template_name = 'travelbuddy/journalentry_form.html'
-    success_url = reverse_lazy('travelbuddy:entry-list') # Redirect after successful creation
+    template_name = 'project/journalentry_form.html'
+    success_url = reverse_lazy('project:entry-list') # Redirect after successful creation
 
     # Automatically set the user to the logged-in user
     def form_valid(self, form):
@@ -37,18 +37,18 @@ class JournalEntryCreateView(LoginRequiredMixin, generic.CreateView):
 class LocationListView(generic.ListView):
     model = Location
     context_object_name = 'location_list'
-    template_name = 'travelbuddy/location_list.html'
+    template_name = 'project/location_list.html'
     queryset = Location.objects.order_by('name')
 
 class LocationDetailView(generic.DetailView):
     model = Location
     context_object_name = 'location'
-    template_name = 'travelbuddy/location_detail.html'
+    template_name = 'project/location_detail.html'
 
 class LocationCreateView(LoginRequiredMixin, generic.CreateView):
     model = Location
-    form_class = LocationForm # Use the ModelForm we will create
-    template_name = 'travelbuddy/location_form.html'
-    success_url = reverse_lazy('travelbuddy:location-list')
+    form_class = LocationForm 
+    template_name = 'project/location_form.html'
+    success_url = reverse_lazy('project:location-list')
 
 
